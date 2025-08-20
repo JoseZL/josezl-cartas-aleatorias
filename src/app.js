eb27263948863c1719a39a0ed6b2c016c28b6742
JoseZL <josezamora485@gmail.com>
2025-08-20 07:46:00 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,38 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const cartaDiv = document.getElementById("carta");
+
+function cartaAleatoria() {
+  const palos = [
+    { nombre: "Corazones", simbolo: "♥", color: "red" },
+    { nombre: "Picas", simbolo: "♠", color: "black" },
+    { nombre: "Tréboles", simbolo: "♣", color: "black" },
+    { nombre: "Diamantes", simbolo: "♦", color: "red" }
+  ];
+  const valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  
+  const palo = palos[Math.floor(Math.random() * palos.length)];
+  const valor = valores[Math.floor(Math.random() * valores.length)];
+
+  return { valor, palo };
+}
+
+function generarCarta() {
+  const { valor, palo } = cartaAleatoria();
+  cartaDiv.innerHTML = `
+    <div class="esquina-arriba" style="color:${palo.color}">${palo.simbolo}</div>
+    <div class="valor-centro">${valor}</div>
+    <div class="esquina-abajo" style="color:${palo.color}">${palo.simbolo}</div>
+  `;
+
+  const ancho = document.getElementById("ancho").value;
+  const alto = document.getElementById("alto").value;
+
+  if (ancho) cartaDiv.style.width = ancho + "px";
+  if (alto) cartaDiv.style.height = alto + "px";
+}
+
+generarCarta();
+
+setInterval(generarCarta, 10000);
